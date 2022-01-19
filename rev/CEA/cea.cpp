c15b5e1b7034ff3e297f0ed23d4307660908a30a
Architect <jabb@itu.dk>
2022-01-19 17:24:27 +0100

    finished cea

--- rev/CEA/cea.cpp
+++ rev/CEA/cea.cpp
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "./cea.h"
 static unsigned char const k8[16] = {	14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7 }; 
 static unsigned char const k7[16] = {	15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10 };
 static unsigned char const k6[16] = {	10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8 };
@@ -16,10 +17,10 @@ unsigned int sb(unsigned int c) {
     char c2 = (c & 0xff00) >> 8;
     char c3 = (c & 0xff0000) >> 16;
     char c4 = (c & 0xff000000) >> 24;
-    unsigned int s1 = k8[c1>>4] | k7[c1 & 15];
-    unsigned int s2 = k6[c2>>4] | k5[c2 & 15];
-    unsigned int s3 = k4[c3>>4] | k3[c3 & 15];
-    unsigned int s4 = k2[c4>>4] | k1[c4 & 15];
+    unsigned char s1 = k8[c1>>4]<<4 | k7[c1 & 15];
+    unsigned char s2 = k6[c2>>4]<<4 | k5[c2 & 15];
+    unsigned char s3 = k4[c3>>4]<<4 | k3[c3 & 15];
+    unsigned char s4 = k2[c4>>4]<<4 | k1[c4 & 15];
     return s1 | (s2<<8) | (s3<<16) | (s4<<24);
 }
 
@@ -30,18 +31,11 @@ int encrypt(char * inp, unsigned int len) {
     if (len % 8 != 0) {
         return -1;
     }
-    for (int i = 0; i < len / 8; i+=8) {
-        printf("%lx\n", *(unsigned long*)(inp+i));
-        unsigned long *t = (unsigned long*)inp+i;
-        unsigned long r = sb((*t << 32)>>32);
+    for (int i = 0; i < len; i+=8) {
+        unsigned long *t = (unsigned long*)(inp+i);
+        unsigned long r = sb((*t & 0xffffffff));
         unsigned long l = sb((*t >> 32));
-        *t = r | l;
-        printf("%lx\n", *(unsigned long*)(inp+i));
+        *t = ((r<<32)^(k & 0xffffffff00000000)) | (l ^ (k & 0xffffffff));
     }
-}
-
-int main(int argc, char ** argv) {
-    char* b = (char*) malloc(8);
-    strncpy(b, "testtest", 8);
-    encrypt(b, 8); 
+    return 0;
 }
---
+++ rev/CEA/cea.h
@@ -0,0 +1,6 @@
+#ifndef cea_h__
+#define cea_h__
+
+extern int encrypt(char *, unsigned int);
+
+#endif
